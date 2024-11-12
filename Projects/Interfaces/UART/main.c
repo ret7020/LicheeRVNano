70b87b6b3e3df6a1112ce158f933a43db47cad7d
ret7020 <ret7020@gmail.com>
2024-11-12 23:28:32 +0300

    UART0 on default image

---
+++ Projects/Interfaces/UART/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+
+#define UART_PATH "/dev/ttyS0"
+
+int main()
+{
+	printf("UART test\n");
+	char serialPort[] = UART_PATH;
+	char txBuf[] = "abcd";
+	struct termios tty;
+	ssize_t writeLen;
+	int serialFd;
+	char rxBuffer[256];
+	int bytesRead;
+
+	serialFd = open(serialPort, O_RDWR | O_NOCTTY);
+	printf("%d\n", serialFd);
+
+	memset(&tty, 0, sizeof(tty));
+
+	// Setting baud
+	cfsetospeed(&tty, B115200);
+	cfsetispeed(&tty, B115200);
+
+	// Generic flags
+	tty.c_cflag &= ~PARENB;
+	tty.c_cflag &= ~CSTOPB;
+	tty.c_cflag &= ~CSIZE;
+	tty.c_cflag |= CS8;
+
+	while (1)
+	{
+		writeLen = write(serialFd, txBuf, sizeof(txBuf));
+		printf("%d\n", writeLen);
+		usleep(50000);
+	}
+
+	return 0;
+}
