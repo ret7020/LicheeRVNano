36e08cd7123844596aefc3c1d5f9c900b007a769
ret7020 <ret7020@gmail.com>
2025-01-17 14:33:27 +0300

    ONNX export script

---
+++ Projects/Yolov8/export_to_onnx.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+import types
+import sys
+
+input_size = (640, 640)
+
+def forward2(self, x):
+  x_reg = [self.cv2[i](x[i]) for i in range(self.nl)]
+  x_cls = [self.cv3[i](x[i]) for i in range(self.nl)]
+  return x_reg + x_cls
+
+model_path = sys.argv[1]
+model = YOLO(model_path)
+model.model.model[-1].forward = types.MethodType(forward2, model.model.model[-1])
+model.export(format='onnx', opset=11, imgsz=input_size)
+
