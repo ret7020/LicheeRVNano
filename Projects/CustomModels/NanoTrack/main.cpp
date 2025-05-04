972aee8c6b59d81c296a400d9d37478c199b97d2
ret7020 <ret7020@gmail.com>
2025-05-04 18:51:20 +0300

    Head running; but not accurate

--- Projects/CustomModels/NanoTrack/main.cpp
+++ Projects/CustomModels/NanoTrack/main.cpp
@@ -15,7 +15,8 @@
 #include <stdint.h>
 #include <string>
 
-void print_tensor_data(const CVI_TENSOR *tensor) {
+void print_tensor_data(const CVI_TENSOR *tensor)
+{
 	int n = tensor->shape.dim[0];
 	int c = tensor->shape.dim[1];
 	int h = tensor->shape.dim[2];
@@ -51,7 +52,8 @@ void print_tensor_data(const CVI_TENSOR *tensor) {
 		exit(1);                          \
 	}
 
-static void ConvertFp32ToBf16(float *src, uint16_t *dst, int count) {
+static void ConvertFp32ToBf16(float *src, uint16_t *dst, int count)
+{
 	for (int i = 0; i < count; ++i)
 	{
 		auto fval = src[i];
@@ -63,7 +65,8 @@ static void ConvertFp32ToBf16(float *src, uint16_t *dst, int count) {
 	}
 }
 
-static void load_input(std::string &input_file, CVI_TENSOR *tensors, int num) {
+static void load_input(std::string &input_file, CVI_TENSOR *tensors, int num)
+{
 	assert(isNpzFile(input_file) && "input should be npz file");
 
 	cnpy::npz_t input_npz = cnpy::npz_load(input_file);
@@ -84,7 +87,57 @@ static void load_input(std::string &input_file, CVI_TENSOR *tensors, int num) {
 	}
 }
 
-class NanoTrack {
+static inline uint16_t float_to_bf16(float f)
+{
+	uint32_t as_int;
+	memcpy(&as_int, &f, sizeof(float));
+	return (uint16_t)(as_int >> 16); // Drop lower 16 bits
+}
+
+void MMMConvertFp32ToBf16(const float *src, uint16_t *dst, size_t count)
+{
+	for (size_t i = 0; i < count; ++i)
+	{
+		dst[i] = float_to_bf16(src[i]);
+	}
+}
+
+static void saveResultToNpz(const std::string &name, CVI_TENSOR *tensors, int32_t num) {
+  assert(isNpzFile(name) && "output should be a npz file");
+
+  cnpy::npz_t npz;
+  for (int i = 0; i < num; i++) {
+    auto &tensor = tensors[i];
+    std::vector<size_t> shape = {(size_t)tensor.shape.dim[0], (size_t)tensor.shape.dim[1],
+                                 (size_t)tensor.shape.dim[2],
+                                 (size_t)tensor.shape.dim[3]};
+    switch (tensor.fmt) {
+      case CVI_FMT_FP32:
+        cnpy::npz_add_array<float>(npz, tensor.name, (float *)CVI_NN_TensorPtr(&tensor), shape);
+        break;
+      case CVI_FMT_BF16: // we use uint16_t to represent BF16
+        cnpy::npz_add_array<uint16_t>(npz, tensor.name, (uint16_t *)CVI_NN_TensorPtr(&tensor),
+                                      shape);
+        break;
+      case CVI_FMT_INT8:
+        cnpy::npz_add_array<int8_t>(npz, tensor.name, (int8_t *)CVI_NN_TensorPtr(&tensor),
+                                    shape);
+        break;
+      case CVI_FMT_UINT8:
+        cnpy::npz_add_array<uint8_t>(npz, tensor.name, (uint8_t *)CVI_NN_TensorPtr(&tensor),
+                                    shape);
+        break;
+      default:
+        TPU_LOG_ERROR("Error, Current unsupported type:%d\n", tensor.fmt);
+        assert(0);
+    }
+  }
+  cnpy::npz_save_all(name, npz);
+}
+
+
+class NanoTrack
+{
 private:
 	// Weights
 	std::string backbone_127_path = "/root/nano/nanotrack_backbone_template_input_quant.cvimodel";
@@ -108,17 +161,20 @@ public:
 	int32_t b255_input_num, b255_output_num;
 	int32_t head_input_num, head_output_num;
 
-	NanoTrack() {
+	NanoTrack()
+	{
 		printf("NanoTrack\n");
 	}
 
-	~NanoTrack() {
+	~NanoTrack()
+	{
 		CVI_NN_CleanupModel(backbone_255_model);
 		CVI_NN_CleanupModel(head_model);
 		// CVI_NN_CleanupModel(backbone_127_model);
 	}
 
-	CVI_RC infer_template_backbone() {
+	CVI_RC infer_template_backbone()
+	{
 		CVI_TENSOR *b127_input_tensors = nullptr;
 		int32_t input_num, output_num;
 
@@ -135,7 +191,8 @@ public:
 		return ret;
 	}
 
-	CVI_RC setup_exampler_backbone() {
+	CVI_RC setup_exampler_backbone()
+	{
 		ret = CVI_NN_RegisterModel(backbone_255_path.c_str(), &backbone_255_model);
 		EXIT_IF_ERROR(ret != CVI_RC_SUCCESS, "failed to register backbone 255 cvimodel");
 
@@ -145,7 +202,8 @@ public:
 		return CVI_NN_GetInputOutputTensors(backbone_255_model, &b255_input_tensors, &b255_input_num, &b255_output_tensors, &b255_output_num);
 	}
 
-	CVI_RC infer_exampler_backbone() {
+	CVI_RC infer_exampler_backbone()
+	{
 		// Load from npz
 		load_input(backbone_255_data_path, b255_input_tensors, b255_input_num);
 
@@ -154,7 +212,8 @@ public:
 		// print_tensor_data(b255_output_tensors);
 	}
 
-	CVI_RC setup_head() {
+	CVI_RC setup_head()
+	{
 		ret = CVI_NN_RegisterModel(head_path.c_str(), &head_model);
 		EXIT_IF_ERROR(ret != CVI_RC_SUCCESS, "failed to register backbone 255 cvimodel");
 
@@ -164,11 +223,33 @@ public:
 		return CVI_NN_GetInputOutputTensors(head_model, &head_input_tensors, &head_input_num, &head_output_tensors, &head_output_num);
 	}
 
-	CVI_RC infer_head() {
-		return 0;
+	CVI_RC infer_head()
+	{
+		// const float* src_fp32 = (const float*)head_input_tensors[0].sys_mem;
+		// uint16_t* dst_bf16 = (uint16_t*)b127_output_tensors->sys_mem;
+		// size_t count = b127_output_tensors->count; // Number of elements (not bytes)
+
+		// MMMConvertFp32ToBf16(src_fp32, dst_bf16, count);
+
+
+		// const float* src_fp32_ = (const float*)head_input_tensors[1].sys_mem;
+		// uint16_t* dst_bf16_ = (uint16_t*)b255_output_tensors->sys_mem;
+		// size_t count_ = b255_output_tensors->count; // Number of elements (not bytes)
+
+		// MMMConvertFp32ToBf16(src_fp32_, dst_bf16_, count_);
+
+		
+		head_input_tensors[0].sys_mem = (uint8_t *)malloc(b127_output_tensors->mem_size);
+		head_input_tensors[1].sys_mem = (uint8_t *)malloc(b255_output_tensors->mem_size);
+
+		memcpy(head_input_tensors[0].sys_mem, b127_output_tensors->sys_mem, b127_output_tensors->mem_size);
+		memcpy(head_input_tensors[1].sys_mem, b255_output_tensors->sys_mem, b255_output_tensors->mem_size);
+		ret = CVI_NN_Forward(head_model, head_input_tensors, head_input_num, head_output_tensors, head_output_num);
+		// print_tensor_data(&head_output_tensors[1]);
+		saveResultToNpz("aaaaa.npz", head_output_tensors, head_output_num);
+		return ret;
 	}
 };
-
 
 int main()
 {
@@ -182,8 +263,8 @@ int main()
 	model.infer_exampler_backbone();
 	model.setup_head();
 
-	print_tensor_data(model.b127_output_tensors);
-	print_tensor_data(model.b255_output_tensors);
+	// print_tensor_data(model.b127_output_tensors);
+	// print_tensor_data(model.b255_output_tensors);
 	model.infer_head();
 
 	// printf("head_input = %d\n", model.head_input_num);
