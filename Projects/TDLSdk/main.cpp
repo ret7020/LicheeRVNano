324b319a2d969bc50333919a64328289ed2a3782
ret7020 <ret7020@gmail.com>
2025-06-29 23:01:07 +0300

    Exp: Upstream version of TDL SDK

---
+++ Projects/TDLSdk/main.cpp
@@ -0,0 +1,14 @@
+#include "cvi_tdl.h"
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
+
+
+cv::VideoCapture cap;
+cvitdl_handle_t tdl_handle = nullptr;
+
+int main() {
+    
+
+}
