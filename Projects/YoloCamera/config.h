2351336e59fd6b2000cbe44d5c2f881917d7073f
ret7020 <ret7020@gmail.com>
2025-06-29 15:38:52 +0300

    Experimental OpenCV Mobile patches

---
+++ Projects/YoloCamera/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+
+#define MODEL_SCALE 0.0039216
+#define MODEL_MEAN 0.0
+#define MODEL_THRESH 0.4
+#define MODEL_NMS_THRESH 0.6
--- Projects/YoloCamera/main.c
+++ Projects/YoloCamera/main.c
@@ -1,43 +1,45 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <chrono>
-#include <functional>
-#include <map>
-#include <sstream>
-#include <string>
-#include <vector>
-#include <signal.h>
 #include "core/cvi_tdl_types_mem_internal.h"
 #include "core/utils/vpss_helper.h"
 #include "cvi_tdl.h"
 #include "cvi_tdl_media.h"
+#include <chrono>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <thread>
+#include <time.h>
+#include <unistd.h>
+#include <algorithm>
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-
+#include "config.h"
 #include "MJPEGWriter.h"
 
-#define MODEL_SCALE 0.0039216
-#define MODEL_MEAN 0.0
-#define MODEL_CLASS_CNT 2
-#define MODEL_THRESH 0.2
-#define MODEL_NMS_THRESH 0.2
-#define BLUE_MAT cv::Scalar(255, 0, 0)
-#define RED_MAT cv::Scalar(0, 0, 255)
+// Globals
+cv::VideoCapture cap;
+cvitdl_handle_t tdl_handle = nullptr;
+volatile sig_atomic_t interrupted = 0;
 
-volatile uint8_t interrupted = 0;
+void clean_up() {
+    cap.release();
+    if (tdl_handle != nullptr) {
+        CVI_TDL_DestroyHandle(tdl_handle);
+        tdl_handle = nullptr;
+    }
+}
 
-void interrupt_handler(int signum)
-{
-    printf("Signal: %d\n", signum);
+void interrupt_handler(int signum) {
+    printf("[MAIN] Signal: %d\n", signum);
     interrupted = 1;
 }
 
-// set preprocess and algorithm param for yolov8 detection
-// if use official model, no need to change param (call this function)
-CVI_S32 init_param(const cvitdl_handle_t tdl_handle)
+CVI_S32 SET_YOLO_Params(const cvitdl_handle_t tdl_handle, int class_cnt)
 {
     // setup preprocess
     YoloPreParam preprocess_cfg =
@@ -60,10 +62,9 @@ CVI_S32 init_param(const cvitdl_handle_t tdl_handle)
         return ret;
     }
 
-    // setup yolo algorithm preprocess
     YoloAlgParam yolov8_param =
         CVI_TDL_Get_YOLO_Algparam(tdl_handle, CVI_TDL_SUPPORTED_MODEL_YOLOV8_DETECTION);
-    yolov8_param.cls = MODEL_CLASS_CNT;
+    yolov8_param.cls = class_cnt;
 
     printf("setup yolov8 algorithm param \n");
     ret =
@@ -82,92 +83,78 @@ CVI_S32 init_param(const cvitdl_handle_t tdl_handle)
     return ret;
 }
 
-int main(int argc, char *argv[])
-{
 
-    signal(SIGINT, interrupt_handler);
-    MJPEGWriter test(7777);
+int main(int argc, char *argv[]) {
 
-    cv::VideoCapture cap;
-    cv::Mat bgr;
+    signal(SIGINT, interrupt_handler); // Correct program stop
+
+    if (argc < 4) {
+        printf("Usage: ./stream_yolo <model_path> <frame_size> <class_cnt>\n");
+        return 0;
+    }
+
+    CVI_S32 ret;
+
+    printf("%s[MAIN] CVI init logs:\n", KYEL);
+
+    cap.set(cv::CAP_PROP_FRAME_HEIGHT, atoi(argv[2]));
+    cap.set(cv::CAP_PROP_FRAME_WIDTH, atoi(argv[2]));
 
     cap.open(0);
-    // cap.set(cv::CAP_PROP_FRAME_WIDTH, 320);
-    // cap.set(cv::CAP_PROP_FRAME_HEIGHT, 320);
-    cap >> bgr;
 
+    cv::Mat bgr;
+
+    MJPEGWriter test(7777);
+    cap >> bgr;
     test.write(bgr);
     test.start();
 
-    printf("Pointer for High-Level code: %p\n", cap.image_ptr);
-    VIDEO_FRAME_INFO_S *frame_ptr = (VIDEO_FRAME_INFO_S *)cap.image_ptr;
-
-    CVI_S32 ret;
-    // VSSGRP already inited by VideoCapture from OpenCV Mobile, second init will do some strange and cause memory problems
-
-    cvitdl_handle_t tdl_handle = NULL;
     ret = CVI_TDL_CreateHandle(&tdl_handle);
-    if (ret != CVI_SUCCESS)
-    {
-        printf("Create tdl handle failed with %#x!\n", ret);
+    if (ret != CVI_SUCCESS) {
+        printf("%s[MAIN] Create tdl handle failed with %#x!%s\n", ret, KRED, KNRM);
         return ret;
     }
 
-    cap >> bgr;
-    // cv::imwrite("captured.jpg", bgr);
-    VIDEO_FRAME_INFO_S frame = *frame_ptr;
-
-    // change param of yolov8_detection
-    ret = init_param(tdl_handle);
-
+    ret = SET_YOLO_Params(tdl_handle, atoi(argv[3]));
     ret = CVI_TDL_OpenModel(tdl_handle, CVI_TDL_SUPPORTED_MODEL_YOLOV8_DETECTION, argv[1]);
 
-    if (ret != CVI_SUCCESS)
-    {
-        printf("open model failed with %#x!\n", ret);
+    if (ret != CVI_SUCCESS) {
+        printf("%s[MAIN] Open model failed with %#x!%s\n", ret, KRED, KNRM);
         return ret;
     }
+    printf("%s[MAIN] Detector ready%s\n", KGRN, KNRM);
 
-    printf("image read,width:%d\n", frame.stVFrame.u32Width);
-    printf("image read,hidth:%d\n", frame.stVFrame.u32Height);
+    while (!interrupted) {
+        cv::Mat frame;
+        std::pair<void *, void *> imagePtrs = cap.capture(frame);
+        void *image_ptr = imagePtrs.first;
 
-    while (!interrupted)
-    {
-        cap >> bgr;
-        VIDEO_FRAME_INFO_S frame = *frame_ptr;
-        cvtdl_object_t obj_meta = {0};
-        // std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-        CVI_TDL_YOLOV8_Detection(tdl_handle, &frame, &obj_meta);
-        // std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-        // double fps = 1 / std::chrono::duration<double>(end - begin).count();
-        // printf("\n\n----------\nDetection FPS: %lf\nDetected objects cnt: %d\n\nDetected objects:\n", fps, obj_meta.size);
-        for (uint32_t i = 0; i < obj_meta.size; i++)
-        {
-            // printf("x1 = %lf, y1 = %lf, x2 = %lf, y2 = %lf, cls: %d, score: %lf\n", obj_meta.info[i].bbox.x1, obj_meta.info[i].bbox.y1, obj_meta.info[i].bbox.x2, obj_meta.info[i].bbox.y2, obj_meta.info[i].classes, obj_meta.info[i].bbox.score);
-            cv::Rect r = cv::Rect(obj_meta.info[i].bbox.x1, obj_meta.info[i].bbox.y1, obj_meta.info[i].bbox.x2 - obj_meta.info[i].bbox.x1, obj_meta.info[i].bbox.y2 - obj_meta.info[i].bbox.y1);
-
-            if (obj_meta.info[i].classes == 0)
-                cv::rectangle(bgr, r, BLUE_MAT, 1, 8, 0);
-            else if (obj_meta.info[i].classes == 1)
-                cv::rectangle(bgr, r, RED_MAT, 1, 8, 0);
-
-            cv::putText(bgr,
-                        "Mat",
-                        cv::Point(obj_meta.info[i].bbox.x1, obj_meta.info[i].bbox.y1 - 5),
-                        cv::FONT_HERSHEY_DUPLEX,
-                        1.0,
-                        (obj_meta.info[i].classes == 0) ? BLUE_MAT : RED_MAT,
-                        1);
+        // Check frame read ok and inference
+        if (image_ptr != nullptr) {
+            VIDEO_FRAME_INFO_S *frameInfo = reinterpret_cast<VIDEO_FRAME_INFO_S *>(image_ptr);
+            cvtdl_object_t obj_meta = {0};
+            CVI_TDL_YOLOV8_Detection(tdl_handle, frameInfo, &obj_meta);
+            cap.releaseImagePtr();
+            image_ptr = nullptr;
+            for (uint32_t i = 0; i < obj_meta.size; i++)
+            {
+                // cv::Mat res;
+                cv::Rect r = cv::Rect(obj_meta.info[i].bbox.x1, obj_meta.info[i].bbox.y1, obj_meta.info[i].bbox.x2 - obj_meta.info[i].bbox.x1, obj_meta.info[i].bbox.y2 - obj_meta.info[i].bbox.y1);
+                // crop object from image
+                // res = bgr(r);
+                // printf("x1 = %lf, y1 = %lf, x2 = %lf, y2 = %lf, cls: %d, score: %lf\n", obj_meta.info[i].bbox.x1, obj_meta.info[i].bbox.y1, obj_meta.info[i].bbox.x2, obj_meta.info[i].bbox.y2, obj_meta.info[i].classes, obj_meta.info[i].bbox.score);
+                cv::rectangle(frame, r, cv::Scalar(50, 255, 100), 1, 8, 0);
+            }
+            test.write(frame);
+            
+            
+        } else { // prevent seg fault
+            interrupted = 1;
         }
-        test.write(bgr);
-        bgr.release();
-    }
+    }    
 
-    printf("Stopping stream:\n");
-    test.stop();
-    cap.release();
+    printf("Exit...\n");
+    clean_up();
 
-    CVI_TDL_DestroyHandle(tdl_handle);
-
-    return ret;
+    return 0;
 }
